13b0bc1d3556d86857e49ed2fce0957ecbc0bb01
vishnunani053 <vishnulucky053@gmail.com>
2023-09-16 00:18:38 +0530

    'okk'

---
+++ newPractise/bulb.js
@@ -0,0 +1,15 @@
+/* function bulbOn(){
+    document.getElementById('img').src='https://media2.giphy.com/media/Lqo3UBlXeHwZDoebKX/giphy.gif'
+}
+let bulbOf=()=>{
+    document.getElementById('img').src="https://bestanimations.com/media/bulbs/2014598748tumblr_nq3a0iw1wH1takhpwo1_500.gif"
+    
+} */
+
+
+function bulbOn(){
+    document.getElementById('img').src='https://media2.giphy.com/media/Lqo3UBlXeHwZDoebKX/giphy.gif'
+}
+let bulbOf=()=>{
+    document.getElementById('img').src='https://bestanimations.com/media/bulbs/2014598748tumblr_nq3a0iw1wH1takhpwo1_500.gif'
+}
---
+++ newPractise/date.js
@@ -0,0 +1,3 @@
+function nani (){
+    document.getElementById("demo").innerHTML=Date()
+}
